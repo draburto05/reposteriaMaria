784812f251d1a5e483137b4e395049ed77909737
Zeltzin03 <zeltzinromo@mail.com>
2025-07-10 16:23:46 -0600

    Creacion de formulario

---
+++ Registro/registro.js
@@ -0,0 +1,9 @@
+document.querySelector('#registro').addEventListener('submit', function(event) { //se manda a traer el formulario de contacto y se agrega el evento que se escuchara al dar clic en el boton
+  event.preventDefault();
+  
+  //se obtienen los valores del formulario y el id de susu validaciones 
+  const name = document.querySelector('#validationNombre').value.trim();  
+  const lastName = document.querySelector('#validationApellido').value.trim();
+  const email = document.querySelector('#validationEmailR').value.trim();
+  const tel = document.querySelector('#validationPhoneR').value.trim();
+  const coment = document.querySelector('#validationPasswordR').value.trim();
